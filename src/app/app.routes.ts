a0ae40e35d0b68481050fffac6749acbedf4b60c
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2024-09-15 18:14:39 -0300

    asdasd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'contact-component', component: ContactComponent },
+
+];
